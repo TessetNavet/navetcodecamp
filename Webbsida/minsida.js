a45e67ae576d477767dea6290b9047c95ddc6173
Johan <epiphe@gmail.com>
2023-02-10 13:03:10 +0100

    JS uppdrag inlagda

---
+++ Webbsida/minsida.js
@@ -0,0 +1,7 @@
+function nyText() {
+    document.getElementById('rubrik2').innerHTML = 'Jag heter Benny.';
+}
+
+function bytBild() {
+    document.getElementById("bild1").src="https://cdn1.epicgames.com/salesEvent/salesEvent/EGS_LeagueofLegends_RiotGames_S1_2560x1440-ee500721c06da3ec1e5535a88588c77f"
+}
