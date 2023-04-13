4ed37a3a81faf5711af2483ce83a05e9da854c51
Tesset Navet <tessetnavet@gmail.com>
2023-04-13 15:04:23 +0200

    alex

--- src/Påsklovet/Alexander H L/minsida.js
+++ src/Påsklovet/Alexander H L/minsida.js
@@ -9,3 +9,35 @@ function nyBild() {
 function nybytBild() {
     document.getElementById('minbild').src = 'https://storage.googleapis.com/pod_public/1300/122734.jpg';
     }
+    var klick = 0;
+  var timerId;
+  
+  function räknaKlick() {
+    klick++;
+    document.getElementById("clickCount").innerHTML = klick;
+    
+    if (!timerId) {
+      var tidKvar = 10; // 5 sekunder
+      document.getElementById("timeRemaining").innerHTML = tidKvar;
+      
+      timerId = setInterval(function () {
+        tidKvar--;
+        document.getElementById("timeRemaining").innerHTML = tidKvar;
+        
+        if (tidKvar == 0) {
+          if (document.getElementById("result").innerHTML < klick) {
+            document.getElementById("result").innerHTML = klick;
+          }
+          nollställKlick();
+        }
+    }, 1000); // 1000 ms = 1 sekund
+  }
+}
+
+function nollställKlick() {
+  klick = 0;
+  document.getElementById("clickCount").innerHTML = klick;
+  clearInterval(timerId);
+  timerId = null;
+  document.getElementById("timeRemaining").innerHTML = "-";
+}
